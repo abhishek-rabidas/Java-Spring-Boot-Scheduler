e1a4d0f2397fb016c0ac6bd0e84f148b850f58aa
abhishek-rabidas <avisec07@gmail.com>
2024-01-31 12:20:18 +0530

    enabled scheduling

--- src/main/java/com/github/viiiraj07/JavaScheduler/JavaSchedulerApplication.java
+++ src/main/java/com/github/viiiraj07/JavaScheduler/JavaSchedulerApplication.java
@@ -2,8 +2,10 @@ package com.github.viiiraj07.JavaScheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class JavaSchedulerApplication {
 
 	public static void main(String[] args) {
