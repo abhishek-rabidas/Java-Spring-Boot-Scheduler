ccca71f33af2456fc6150b3807e039078abe329d
abhishek-rabidas <avisec07@gmail.com>
2024-01-31 12:19:57 +0530

    project init

---
+++ src/main/java/com/github/viiiraj07/JavaScheduler/JavaSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.github.viiiraj07.JavaScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/viiiraj07/JavaScheduler/JavaSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.viiiraj07.JavaScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
